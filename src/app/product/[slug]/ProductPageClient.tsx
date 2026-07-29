9a381b242f2e145c3933ba0f16316ca3b75999c8
cravecom64-cmyk <cravecom64@gmail.com>
2026-07-29 15:25:35 +0300

    Add files via upload

--- src/app/product/[slug]/ProductPageClient.tsx
+++ src/app/product/[slug]/ProductPageClient.tsx
@@ -417,7 +417,7 @@ export function ProductPageClient() {
           <h3 className="text-[13px] font-extrabold">🛍 З цим купують</h3>
           <div className="flex gap-2 mt-2 overflow-x-auto pb-2 scrollbar-hide">
             {crossSell.map((p) => (
-              <div key={p.id} className="min-w-[140px] flex-shrink-0">
+              <div key={p.id} className="w-[140px] flex-shrink-0">
                 <ProductCard
                   id={p.id}
                   slug={p.slug}

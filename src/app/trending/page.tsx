2f73a4d555af4dfbed0eb08b9ba8c9aa99604f9c
cravecom64-cmyk <cravecom64@gmail.com>
2026-08-04 13:55:48 +0300

    Add files via upload

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -32,6 +32,8 @@ export default function TrendingPage() {
             id={product.id}
             slug={product.slug}
             name={product.name}
+            category={product.category}
+            categoryName={product.categoryName}
             price={product.price}
             oldPrice={product.oldPrice}
             emoji={product.emoji}
